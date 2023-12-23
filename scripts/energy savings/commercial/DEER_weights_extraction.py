fa4112356a12c9e2a1aeb0fdebbaec0fc7552053
Yitian <yitian.liang@dnv.com>
2023-12-23 00:03:10 +0000

    Create DEER_weights_extraction.py
    to be worked on

---
+++ scripts/energy savings/commercial/DEER_weights_extraction.py
@@ -0,0 +1,39 @@
+#%%
+import pandas as pd
+import numpy as np
+import os
+import sys
+import datetime as dt
+
+os.chdir(os.path.dirname(__file__)) #resets to current script directory
+
+import sqlalchemy
+import psycopg2
+#%%
+from sqlalchemy import create_engine, text
+# %%
+#read from DEER database
+
+engine = sqlalchemy.create_engine("postgresql://sptviewer:deereddev@cpucexante.cwuiixjcexyp.us-east-1.rds.amazonaws.com:5432/DEER")
+
+
+#%%
+db_params = {
+    'host': 'cpucexante.cwuiixjcexyp.us-east-1.rds.amazonaws.com',
+    'database': 'DEER',
+    'user': 'sptviewer',
+    'password': 'deereddev',
+    'port': '5432'
+}
+
+conn = psycopg2.connect(**db_params)
+
+#12/22/2023 connection error?
+# %%
+query = text('''
+SELECT *
+FROM applic.BldgWts;
+''')
+
+df = pd.read_sql_table(query, engine.connect())
+# %%
