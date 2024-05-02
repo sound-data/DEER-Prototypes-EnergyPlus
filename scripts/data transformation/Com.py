ad62373f78c5408dbb6a15cc8ee581dee766ef99
Justin-DeBlois <jdeblois@trccompanies.com>
2024-05-02 13:28:33 -0400

    Add refrigeration end use column to com.py

--- scripts/data transformation/Com.py
+++ scripts/data transformation/Com.py
@@ -247,7 +247,7 @@ def end_use_rearrange(df_in):
 
     df_in['kwh_venthtg'] =0 #placeholders fields for now
     df_in['kwh_ventclg'] =0
-    df_in['kwh_refg'] = 0 
+    df_in['kwh_refg'] = df_in['Refrigeration (kWh)']
     df_in['kwh_hpsup'] = 0
     df_in['kwh_shw'] = 0
     df_in['kwh_ext'] = 0
