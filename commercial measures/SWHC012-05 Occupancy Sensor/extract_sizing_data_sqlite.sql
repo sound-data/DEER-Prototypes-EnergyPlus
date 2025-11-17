bd3f05714a3ac2001e420f9b9676f24c6224e354
Nicholas Fette <nfette@gmail.com>
2025-11-17 08:47:07 -0800

    Helper file for use with result2.py to get sizing data

---
+++ commercial measures/SWHC012-05 Occupancy Sensor/extract_sizing_data_sqlite.sql
@@ -0,0 +1,24 @@
+-- For use with result2.py -s -t
+-- Extract sizing data table compatible with output from result.py
+SELECT
+t."TabularDataIndex",
+CAST(t."Value" as REAL),
+t."ReportName",
+t."ReportForString",
+t."TableName",
+t."RowName",
+t."ColumnName",	
+t."Units",
+m."File Name",
+m."BldgLoc", 
+m."BldgType", 
+m."Story", 
+m."BldgHVAC", 
+m."BldgVint", 
+m."TechGroup", 
+m."TechType", 
+m."TechID"
+from sim_metadata m
+join sim_tabular t
+on t."filename" = m."File Name"
+;
